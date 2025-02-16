204dfc7b0ae511c3914501b538a6d5babba19a73
avery.white <averydouglascwhite@usf.edu>
2025-02-16 15:37:00 -0500

    Task 2 - Product Price after Discount

--- cc_8.js
+++ cc_8.js
@@ -9,3 +9,12 @@ console.log(calculateSalary(5000, 500, 0.1)); // Expected output: Your salary is
 console.log(calculateSalary(7000, 1000, 0.15)); // Expected output: Your salary is $5955.00
 // Task 1 - End
 
+// Task 2 - Product Price after discount
+function calculateDiscount(price, discountRate) { // Calculate the final price after applying the discount
+    let finalPrice = price - (price * discountRate); // Math to calculate the final price
+    return `The final price is $${finalPrice.toFixed(2)}`; // Return the final price with 2 decimal points
+}
+console.log(calculateDiscount(100, 0.2)); // Expected output: The final price is $80.00
+console.log(calculateDiscount(250, 0.15)); // Expected output: The final price is $212.50
+// Task 2 - End
+
